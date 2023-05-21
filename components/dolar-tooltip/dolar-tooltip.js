5275fe246b078e6a029e49d0064685b68fa70110
Diego Gandara <diego.gandara@pedidosya.com>
2023-05-21 19:21:05 -0300

    funcionalidad: se crea el icono del dolar-tooltip y se inserta en la pagina de servicios. Asimismo, se linkea un dialog (modal) para que cuando la informacion de la pegada -la promesa- se resuelva, se muestre en ese modal.

---
+++ components/dolar-tooltip/dolar-tooltip.js
@@ -0,0 +1,43 @@
+const $dolarIcon = document.querySelector('#dolar-tooltip');
+const $dolarOficial = document.querySelector('#dolar-oficial');
+const $dolarBlue = document.querySelector('#dolar-blue');
+const $dolarModal = document.querySelector('[data-modal]');
+const $closeButtonModal = document.querySelector('[data-close-modal]');
+
+// API
+const API_URL = 'https://www.dolarsi.com/api/api.php?type=valoresprincipales';
+
+const getDolarValues = async () => {
+  const data = await fetch(API_URL);
+  const res = await data.json();
+  return await res;
+};
+
+$dolarIcon.addEventListener('click', () => {
+  console.log('Se hizo click');
+  $dolarModal.show();
+
+  let promesaDolar = (async function () {
+    return await getDolarValues();
+  })();
+
+  let dolarOficial = null;
+  let dolarBlue = null;
+
+  $dolarBlue.innerHTML = 'Estamos cargando la data, por favor espere.';
+
+  promesaDolar.then((res) => {
+    const oficial = res.find((dolar) => dolar.casa.nombre === 'Dolar Oficial');
+    const blue = res.find((dolar) => dolar.casa.nombre === 'Dolar Blue');
+
+    dolarOficial = oficial && oficial;
+    dolarBlue = blue && blue;
+
+    $dolarBlue.innerHTML = `<strong>Dolar blue</strong>: Compra $${dolarBlue?.casa?.compra} - Venta $${dolarBlue?.casa.venta}`;
+    $dolarOficial.innerHTML = `<strong>Dolar oficial</strong>: Compra $${dolarOficial?.casa?.compra} - Venta $${dolarOficial?.casa.venta}`;
+  });
+});
+
+$closeButtonModal.addEventListener('click', () => {
+  $dolarModal.close();
+});
